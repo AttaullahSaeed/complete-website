cb48f2f3fe5e183d47ef25779f75450c6b3ae5c6
AttaullahSaeed <attaullahsaeed77@gmail.com>
2020-09-05 01:29:13 +0500

    Updates

---
+++ precache-manifest.0b73ee31e51a528afb15140a6573dafb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee8affc8f31393213db5ab15fa5fe62",
+    "url": "/complete-website/index.html"
+  },
+  {
+    "revision": "df6625123b013836c20d",
+    "url": "/complete-website/static/css/main.d08f7be5.chunk.css"
+  },
+  {
+    "revision": "40fdb3713b59222f84d5",
+    "url": "/complete-website/static/js/2.52c6cdef.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/complete-website/static/js/2.52c6cdef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6625123b013836c20d",
+    "url": "/complete-website/static/js/main.cdf42e81.chunk.js"
+  },
+  {
+    "revision": "6fccf80927e82d9ed470",
+    "url": "/complete-website/static/js/runtime-main.58c66b19.js"
+  },
+  {
+    "revision": "b4daaa569686dc4c695696015f6b4181",
+    "url": "/complete-website/static/media/ars.b4daaa56.jpg"
+  },
+  {
+    "revision": "fbd87e1bb1a75c97912e2603d201c9c7",
+    "url": "/complete-website/static/media/hero-bg.fbd87e1b.png"
+  },
+  {
+    "revision": "f636c6b7dbf5ef324a35971e13d7776a",
+    "url": "/complete-website/static/media/img-1.f636c6b7.png"
+  },
+  {
+    "revision": "c641b8d6b7eb9c8c35c753574d5edc9d",
+    "url": "/complete-website/static/media/img-2.c641b8d6.png"
+  },
+  {
+    "revision": "efb35778c9d2ca74d48557fc5cf5edb5",
+    "url": "/complete-website/static/media/m4.efb35778.jpg"
+  },
+  {
+    "revision": "ac8767c62548c92d492c0f38edda3471",
+    "url": "/complete-website/static/media/me3.ac8767c6.jpg"
+  },
+  {
+    "revision": "898a0f7d68ab4f3d06d5914765945de9",
+    "url": "/complete-website/static/media/pic2.898a0f7d.jfif"
+  },
+  {
+    "revision": "70d8e960f0d153334f5c34fccfb09989",
+    "url": "/complete-website/static/media/pic3.70d8e960.jfif"
+  },
+  {
+    "revision": "c36cca14f741e2600b2fd65d88961061",
+    "url": "/complete-website/static/media/pic4.c36cca14.jfif"
+  },
+  {
+    "revision": "2fb467d6ad821f37ab8775f8fbc9c912",
+    "url": "/complete-website/static/media/tahir.2fb467d6.jpg"
+  },
+  {
+    "revision": "84e7ff4705f6a9d67ce486fe7dbc376e",
+    "url": "/complete-website/static/media/umar.84e7ff47.jpg"
+  }
+]);
